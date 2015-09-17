1154a89159f4fc0f490c9770e301879df93eebb7
Bill Walker <bwalker@mozilla.com>
2015-09-16 21:58:02 -0700

    Updates

--- js/app.js
+++ js/app.js
@@ -1,13 +1,29 @@
 // app.js
 
+var gAudioContext = new AudioContext();
+var gOscillator = null;
+
 $(document).ready(function() {
 	$('button').click(function (e) {
 		e.preventDefault();
-		console.log('clicked play', e.target.getAttribute('id'));
-	})
+		$(this).toggleClass('checked');
+		console.log('clicked play', $(this).hasClass('checked'));
+
+		if ($(this).hasClass('checked')) {
+			gOscillator = gAudioContext.createOscillator();
+			gOscillator.frequency.value = 200;
+			gOscillator.connect(gAudioContext.destination);
+			gOscillator.start(0);	
+		} else {
+			gOscillator.stop(0);
+		}
+
+	});
 
 	$(document).on('change', 'input:radio', function (e) {
 		e.preventDefault();
 		console.log('clicked selector', e.target.id);
-	})
+	});
+
+
 });
