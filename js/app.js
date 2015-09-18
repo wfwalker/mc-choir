f722d608b76ad3a16da0afea19fae667d801877a
Bill Walker <bwalker@mozilla.com>
2015-09-18 11:19:26 -0700

    playback a buffer instead of an oscillator

--- js/app.js
+++ js/app.js
@@ -1,7 +1,38 @@
 // app.js
 
 var gAudioContext = new AudioContext();
-var gOscillator = null;
+var gSoundSource = null;
+
+setBufferFromURL = function(inSoundDataURL) {
+	console.log('setBufferFromURL ' + inSoundDataURL);
+
+	var mp3Request = new XMLHttpRequest();
+
+	mp3Request.onerror = function(e) {
+		console.log('error downloading');
+	}.bind(this);
+
+	mp3Request.onprogress = function(e) {
+		console.log(Math.round(100 * e.loaded / e.total) + '%');
+	}.bind(this);
+
+	mp3Request.onload = function(e) {
+		    gAudioContext.decodeAudioData(mp3Request.response, function(decodedBuffer) {
+
+			gSoundSource = gAudioContext.createBufferSource();
+			gSoundSource.connect(gAudioContext.destination);
+			gSoundSource.buffer = decodedBuffer;
+
+			// start playing immediately in a loop
+			gSoundSource.loop = true;
+			gSoundSource.start(0);
+		}.bind(this));
+	}.bind(this);
+
+	mp3Request.open("GET", inSoundDataURL, true);
+	mp3Request.responseType = 'arraybuffer';
+	mp3Request.send();
+}
 
 $(document).ready(function() {
 	$('button').click(function (e) {
@@ -13,14 +44,12 @@ $(document).ready(function() {
 			console.log('START',
 				$(this).parent().parent().find('.btn-group label.active input')[0].id
 			);
-			gOscillator = gAudioContext.createOscillator();
-			gOscillator.frequency.value = 200;
-			gOscillator.connect(gAudioContext.destination);
-			gOscillator.start(0);	
+
+			setBufferFromURL("./saw440.mp3");
 		} else {
 			console.log('STOP');
-			gOscillator.stop(0);
-			gOscillator.disconnect();
+			gSoundSource.stop(0);
+			gSoundSource.disconnect();
 		}
 
 	});
