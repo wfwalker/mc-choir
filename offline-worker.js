5dfb90f9ef62c737538d8128b709a884a31373cf
Travis CI <bwalker@mozilla.com>
2016-02-26 06:29:52 +0000

    Updates

--- offline-worker.js
+++ offline-worker.js
@@ -37,7 +37,7 @@
 
     // This is the unique name for the cache controlled by this version of the worker.
     get CACHE_NAME() {
-      return this.CACHE_PREFIX + '2d1f5e992020165740c1c539abd4cf3eea6db997';
+      return this.CACHE_PREFIX + '02b64dad686f6986efdf786d952c2894f7eeb63e';
     },
 
     // This is a list of resources that will be cached.
@@ -89,13 +89,11 @@
       './fonts/glyphicons-halflings-regular.svg', // 796e58aedfcfe8a3b0829bc0594f739936a9d7d0
       './fonts/glyphicons-halflings-regular.ttf', // c427041d38cd6597ae7e758028ab72756849ec26
       './fonts/glyphicons-halflings-regular.woff', // c707207e52ffe555a36880e9873d146c226e3533
-      './js/app.js', // ca4cef6230d1463414db772c04e32a8c786ebe24
-      './js/birdSongPlayer.js', // 0905a106cb3e04472067a08d353ccd5d6d51bb94
+      './js/app.js', // 114e8f464806fa5461238b7368cd71fc4df5deed
       './js/bootstrap.js', // 8c639912ccd43078865578e598607d1b847c2373
       './js/fastclick.js', // 06cef196733a710e77ad7e386ced6963f092dc55
       './js/jquery.min.js', // ae49e56999d82802727455f0ba83b63acd90a22b
       './js/offline-manager.js', // 66eee9a121acf84e027e87bc73264faaa900ca8b
-      './js/placeTimeBirdSongs.js', // d8074835d26825c23b719010cd63e709755df789
 
     ],
 
